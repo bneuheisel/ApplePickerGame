f9405dfcf7ec720b7f28649e074fdd67cf45e368
Brandon <bneuheisel01@my.gcu.edu>
2019-10-01 19:57:51 -0700

    Add game project
    Adding finished game

---
+++ New Unity Project/Assets/Scenes/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+    public void Play()
+    {
+        //SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Space");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Night");
+    }
+}
---
+++ New Unity Project/Assets/Scenes/Scripts/NewSkybox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSkybox : MonoBehaviour
+{
+    public Material skybox;
+    void Awake()
+    {
+        RenderSettings.skybox = skybox;
+    }
+}
